25cd5b5907bc1b33b13b537a34cd50371882564d
Nate Voisan <68405756+NateVoisan@users.noreply.github.com>
2020-09-26 22:38:28 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import api from './api';
+// import 'normalize.css';
+import './index.css';
+import STORE from './store.js';
+import bookmarks from './bookmarks.js'
+
+
+
+const main = function() {
+    api.getBookmarks()
+    .then(response => response.json())
+    .then(response => {
+        response.forEach(bookmark => STORE.addBookmark(bookmark))
+        bookmarks.render()
+    })
+    bookmarks.bindEventListeners();
+}
+
+$(main);
---
+++ src/store.js
@@ -0,0 +1,68 @@
+let bookmarks = [];
+let adding = false;
+let error = null;
+let filter = 0;
+let filteredBookmarks = [];
+
+const addBookmark = function(bookmark) {
+    for (let i = 0; i < bookmarks.length; i++)
+    if (bookmarks[i]) {
+        bookmarks[i].expand = false;
+    }
+    bookmarks.push(bookmark);
+    this.adding = false;
+}
+
+const setAdding = function(param) {
+    this.adding = param;
+}
+
+const expandBookmark = function(id) {
+    let expandedBookmark = bookmarks.find(bookmark => bookmark.id === id)
+    if (expandedBookmark.expand) {
+        expandedBookmark.expand = false;
+    } else {
+        expandedBookmark.expand = true;
+    }
+}
+
+const deleteBookmark = function(id) {
+    this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id);
+}
+
+const filterBookmarks = function(filterNumber) {
+    this.filter = true;
+    this.bookmarks.forEach(bookmark => {
+        if (bookmark.rating >= filterNumber) {
+            this.filteredBookmarks.push(bookmark)
+        }
+    })
+}
+
+const findById = function (id) {
+    return this.bookmarks.find(currentItem => currentItem.id === id);
+}
+
+const setFiltering = function(param) {
+    this.filter = param;
+}
+
+const setError = function(errorMessage) {
+    this.error = errorMessage;
+}
+
+export default {
+    findById,
+    bookmarks,
+    adding,
+    error,
+    filter,
+    addBookmark,
+    expandBookmark,
+    deleteBookmark,
+    setAdding,
+    setFiltering,
+    setError,
+    filterBookmarks,
+    filteredBookmarks
+}
